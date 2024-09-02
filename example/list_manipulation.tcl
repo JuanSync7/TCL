e91feef89a5a46c9519f798fb59007cbb5434ed7
JuanSync7 <118364808+JuanSync7@users.noreply.github.com>
2024-09-02 15:38:33 +0100

    Update list_manipulation.tcl

--- example/list_manipulation.tcl
+++ example/list_manipulation.tcl
@@ -14,5 +14,10 @@ puts [lindex $list_name $var]
 # get the length of list_name
 puts [llength $list_name]
 
-set dict_name [ dict ]
+# set var 2 to "abc 123 def 456"
+set var2 "abc 123 def 456"
 
+# split var 2 using space delimiter and store it in list 3
+set list3 [split $var2 \ ]
+
+puts list3
