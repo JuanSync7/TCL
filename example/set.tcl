9a271483387b3cbad2120343e94c3a04a387357a
JuanSync7 <118364808+JuanSync7@users.noreply.github.com>
2024-09-02 15:23:12 +0100

    Update and rename set.tcl to useful_set_cmd.tcl

--- example/set.tcl
+++
@@ -1,8 +0,0 @@
-# set variable_name to a
-set variable_name a
-
-# set list_name as an empty list
-set list_name [list ]
-
-# set the first variable of list_name to a
-set list_name [list a]
---
+++ example/useful_set_cmd.tcl
@@ -0,0 +1,19 @@
+# set variable_name to a
+set variable_name a
+
+# set new_variable to variable_name
+set new_variable $variable_name
+
+# set list_name as an empty list
+set list_name [list ]
+
+# set the first variable of list_name to a
+set list_name [list a]
+
+# set string_name to abc
+set string_name [string abc]
+
+# concatinate a string_name and string literal "123" to combined_string (abc123)
+set combined_string [string cat "$string_name" "123"]
+
+ 
